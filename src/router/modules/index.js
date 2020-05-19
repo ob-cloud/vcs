99e09b0e5f33218b732656779b3035b7f325839e
eamiear <justcall_me_k@163.com>
2020-05-19 17:03:37 +0800

    路由入口修改

--- src/router/modules/index.js
+++ src/router/modules/index.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-08-05 11:26:26
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-12-03 17:24:47
+ * @Last Modified time: 2020-05-19 16:47:28
  */
 
 import common from './common'
@@ -18,7 +18,7 @@ const menuRoutes = {
       name: 'dashboard',
       path: '/dashboard',
       component: () => import('views/dashboard/index.vue'),
-      redirect: { path: '/system/version.html' },
+      redirect: { path: '/system/room.html' },
     },
     ...navlist,
     ...common
